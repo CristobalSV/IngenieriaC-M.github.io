672a2d6d9605cab5452dad534c0616eabef44b47
CristobalSV <crixpo56@gmail.com>
2023-11-17 16:15:02 -0300

    3.2.0

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// scripts.js
+
+var botonFlotante = document.getElementById('boton-flotante');
+var redesSociales = document.getElementById('redes-sociales');
+
+botonFlotante.addEventListener('click', toggleRedesSociales);
+
+function toggleRedesSociales() {
+    redesSociales.classList.toggle('mostrar');
+}
 
 let descripciones = [
     "Hola, ¿Quieres cotizar con nosotros?",
