f2633cc7d17cbbee57653d3970db68d021a80c03
CristobalSV <crixpo56@gmail.com>
2023-11-20 17:00:31 -0300

    3.2.2

--- script.js
+++ script.js
@@ -7,13 +7,3 @@ botonFlotante.addEventListener('click', toggleRedesSociales);
 function toggleRedesSociales() {
     redesSociales.classList.toggle('mostrar');
 }
-
-let descripciones = [
-    "Hola, ¿Quieres cotizar con nosotros?",
-    "Esta es la descripción de 'Nuestra Misión'."
-];
-
-function cotizarProducto(cardIndex) {
-    // Implementa la acción de cotización aquí, por ejemplo, abrir un formulario de cotización en una nueva ventana.
-    window.open(`https://wa.me/+56941123987?text=${encodeURIComponent(descripciones[cardIndex])}`, "_blank");
-}
